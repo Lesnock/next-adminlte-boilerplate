6d420b6231a47217776cc1973909853f208c5382
METADIL MÓVEIS - Caio Lesnock <caio@metadil.com.br>
2020-12-02 16:47:58 -0300

    Add ordering to table

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -1,149 +1,125 @@
-// import { Container } from './styles';
+import { useEffect, useState } from 'react'
+import router from 'next/router'
 
-export default function Table() {
+import styles from './Styles.module.css'
+
+type Header = {
+  label: string
+  name: string
+  sortable: boolean
+}
+
+type Row = Array<string | number>
+
+type TableProps = {
+  title?: string
+  headers?: Header[]
+  rows?: Row[]
+}
+
+export default function Table({ title, headers = [], rows = [] }: TableProps) {
+  const { sort, order } = router.query
+
+  const [ordering, setOrdering] = useState({
+    sort: sort,
+    order: order || 'asc'
+  })
+
+  // Sort table
+  const sortBy = (column: string) => {
+    if (ordering.sort === column && ordering.order === 'asc') {
+      return setOrdering({ sort: column, order: 'desc' })
+    }
+
+    return setOrdering({ sort: column, order: 'asc' })
+  }
+
+  // Get header icon according to header state
+  const getSortableIcon = (header: Header) => {
+    if (!header.sortable) {
+      return ''
+    }
+
+    if (header.name !== ordering.sort) {
+      return 'fas fa-arrows-alt-v'
+    }
+
+    if (ordering.order === 'desc') {
+      return 'fas fa-arrow-up'
+    }
+
+    return 'fas fa-arrow-down'
+  }
+
+  // React to ordering changes
+  useEffect(() => {
+    console.log(ordering)
+  }, [ordering])
+
   return (
-    <div className="row">
-      <div className="col-12">
-        <div className="card">
-          <div className="card-header">
-            <h3 className="card-title">Fixed Header Table</h3>
-            <div className="card-tools">
-              <div
-                className="input-group input-group-sm"
-                style={{ width: 150 }}
-              >
-                <input
-                  type="text"
-                  name="table_search"
-                  className="form-control float-right"
-                  placeholder="Search"
-                />
-                <div className="input-group-append">
-                  <button type="submit" className="btn btn-default">
-                    <i className="fas fa-search" />
-                  </button>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-12">
+          <div className="card">
+            <div className="card-header">
+              <h3 className="card-title">{title}</h3>
+              <div className="card-tools">
+                <div
+                  className="input-group input-group-sm"
+                  style={{ width: 300 }}
+                >
+                  <input
+                    type="text"
+                    name="table_search"
+                    className="form-control float-right"
+                    placeholder="Search"
+                  />
+                  <div className="input-group-append">
+                    <button type="submit" className="btn btn-default">
+                      <i className="fas fa-search" />
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
+
+            <div className="card-body table-responsive p-0">
+              <table className="table table-head-fixed text-nowrap">
+                <thead>
+                  <tr>
+                    {headers &&
+                      headers.map((header, index) => (
+                        <th
+                          key={index}
+                          className={header.sortable ? styles.sortable_th : ''}
+                          onClick={() => {
+                            if (header.sortable) sortBy(header.name)
+                          }}
+                        >
+                          {header.label}
+                          {header.sortable && (
+                            <i
+                              className={`fas ${getSortableIcon(header)}
+                              ${styles.sortable_icon}`}
+                            />
+                          )}
+                        </th>
+                      ))}
+                  </tr>
+                </thead>
+                <tbody>
+                  {rows.map((row, rowIndex) => (
+                    <tr key={rowIndex}>
+                      {row.map((cell, cellIndex) => (
+                        <td key={cellIndex}>{cell}</td>
+                      ))}
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
           </div>
-          {/* /.card-header */}
-          <div
-            className="card-body table-responsive p-0"
-            style={{ height: 300 }}
-          >
-            <table className="table table-head-fixed text-nowrap">
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>User</th>
-                  <th>Date</th>
-                  <th>Status</th>
-                  <th>Reason</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>183</td>
-                  <td>John Doe</td>
-                  <td>11-7-2014</td>
-                  <td>
-                    <span className="tag tag-success">Approved</span>
-                  </td>
-                  <td>
-                    Bacon ipsum dolor sit amet salami venison chicken flank
-                    fatback doner.
-                  </td>
-                </tr>
-                <tr>
-                  <td>219</td>
-                  <td>Alexander Pierce</td>
-                  <td>11-7-2014</td>
-                  <td>
-                    <span className="tag tag-warning">Pending</span>
-                  </td>
-                  <td>
-                    Bacon ipsum dolor sit amet salami venison chicken flank
-                    fatback doner.
-                  </td>
-                </tr>
-                <tr>
-                  <td>657</td>
-                  <td>Bob Doe</td>
-                  <td>11-7-2014</td>
-                  <td>
-                    <span className="tag tag-primary">Approved</span>
-                  </td>
-                  <td>
-                    Bacon ipsum dolor sit amet salami venison chicken flank
-                    fatback doner.
-                  </td>
-                </tr>
-                <tr>
-                  <td>175</td>
-                  <td>Mike Doe</td>
-                  <td>11-7-2014</td>
-                  <td>
-                    <span className="tag tag-danger">Denied</span>
-                  </td>
-                  <td>
-                    Bacon ipsum dolor sit amet salami venison chicken flank
-                    fatback doner.
-                  </td>
-                </tr>
-                <tr>
-                  <td>134</td>
-                  <td>Jim Doe</td>
-                  <td>11-7-2014</td>
-                  <td>
-                    <span className="tag tag-success">Approved</span>
-                  </td>
-                  <td>
-                    Bacon ipsum dolor sit amet salami venison chicken flank
-                    fatback doner.
-                  </td>
-                </tr>
-                <tr>
-                  <td>494</td>
-                  <td>Victoria Doe</td>
-                  <td>11-7-2014</td>
-                  <td>
-                    <span className="tag tag-warning">Pending</span>
-                  </td>
-                  <td>
-                    Bacon ipsum dolor sit amet salami venison chicken flank
-                    fatback doner.
-                  </td>
-                </tr>
-                <tr>
-                  <td>832</td>
-                  <td>Michael Doe</td>
-                  <td>11-7-2014</td>
-                  <td>
-                    <span className="tag tag-primary">Approved</span>
-                  </td>
-                  <td>
-                    Bacon ipsum dolor sit amet salami venison chicken flank
-                    fatback doner.
-                  </td>
-                </tr>
-                <tr>
-                  <td>982</td>
-                  <td>Rocky Doe</td>
-                  <td>11-7-2014</td>
-                  <td>
-                    <span className="tag tag-danger">Denied</span>
-                  </td>
-                  <td>
-                    Bacon ipsum dolor sit amet salami venison chicken flank
-                    fatback doner.
-                  </td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          {/* /.card-body */}
         </div>
-        {/* /.card */}
       </div>
     </div>
   )
--- src/pages/produtos.tsx
+++ src/pages/produtos.tsx
@@ -1,3 +1,4 @@
+import Table from '../components/Table'
 import AdminLayout from '../layouts/admin'
 import { BreadcrumbItem } from '../components/Breadcrumb'
 
@@ -7,11 +8,22 @@ export default function Produtos() {
     { name: 'Produtos', link: '/produtos', active: true }
   ]
 
+  const headers = [
+    { label: 'ID', name: 'id', sortable: true },
+    { label: 'Nome', name: 'name', sortable: true }
+  ]
+  const rows = [
+    [1, 'Caio Lesnock'],
+    [2, 'Gabriela Lesnock']
+  ]
+
   return (
     <AdminLayout
       title="Produtos"
       actives={['products']}
       breadcrumb={breadcrumb}
-    ></AdminLayout>
+    >
+      <Table headers={headers} rows={rows} />
+    </AdminLayout>
   )
 }
